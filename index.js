a087f87e9ca54698d7213706f6b469df0f16a63e
rtardo <84642031+rtardo@users.noreply.github.com>
2022-11-24 21:23:28 +0300

    GET user's exercise log, filter exercise list

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ app.post('/api/users', async (req, res) => {
     name : userName
   });
   await userInstance.save();
-  res.json({username : userName});
+  res.json({username : userName,
+    _id : userInstance._id
+  });
 });
 
 app.post('/api/users/:_id/exercises', async (req, res) => {
@@ -32,13 +34,15 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
     date : req.body.date,
   };
   try {
-    await user.findByIdAndUpdate(userId,
+    let userInstance = await user.findByIdAndUpdate(userId,
       {'$push': {'exerciseList': {description : req.body.description,
         duration : req.body.duration,
-        date : req.body.date,}}},
-      { "new": true, "upsert": true });
+        date : req.body.date
+      }}},
+      { "new": true, "upsert": true 
+    });
 
-      let userInstance = await user.findByIdAndUpdate(userId);
+      //let userInstance = await user.findByIdAndUpdate(userId);
       //res.json(userInstance.exerciseList.length);
       //res.json(userInstance);
       res.json({
@@ -55,11 +59,54 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
 });
 
 app.get('/api/users/:_id/logs', async (req, res) => {
-  res.json({test: req.query.limit});
+  //res.json({test: req.query.limit});
+  const userId = req.params._id;
   console.log(req.query.limit);
   console.log(typeof req.query.limit);
+  try {
+
+      let userInstance = await user.findByIdAndUpdate(userId);
+      let exerciseList =  filterExercises(req.query.from, req.query.to, req.query.limit, userInstance.exerciseList);
+      console.log(exerciseList);
+      res.json({
+        _id : userId,
+        username : userInstance.name,
+        count : exerciseList.length,
+        log : exerciseList,
+        //log : exerciseList,
+      });
+  
+  }
+  catch(error) {
+    res.json(error);
+  }
   
 });
+
+let filterExercises = (from, to, limit, exerciseList) => {
+  let filteredList = exerciseList;
+
+  if (from !== undefined) filteredList = filteredList.filter(exercise => new Date(exercise.date) > new Date(from));
+  if (to !== undefined) filteredList = filteredList.filter(exercise => new Date(exercise.date) < new Date(to));
+  if (limit !== undefined) filteredList = filteredList.slice(0,limit);
+  //console.log(filteredList);
+  filteredList = filteredList.map((item) => {
+    return {
+    description: item.description,
+    duration: item.duration,
+    date: new Date(item.date).toDateString()
+  }});
+/*
+  for (let item of filteredList){
+    exercise = {
+      description: item.description,
+      duration: item.duration,
+      date: item.date
+    };
+  }
+  */
+  return filteredList;
+}
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
